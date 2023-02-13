Fixed sparse dot vecotr with PSI leakage

experiments.py
@@ -379,8 +379,8 @@ class SparseVectorORAM(SecureMatrix):
 
 
 async def main():
-    n_dim = 1000000
-    density = 0.001
+    n_dim = 1000
+    density = 0.1
     secint = mpc.SecInt(64)
 
     x_sparse = scipy.sparse.random(n_dim, 1, density=density, dtype=np.int16).astype(
@@ -393,27 +393,26 @@ async def main():
     dense_x = x_sparse.astype(int).todense()
     dense_y = y_sparse.astype(int).todense()
     print("Real result:", dense_x.transpose().dot(dense_y)[0, 0])
-    print("===")
     sec_dense_x = DenseVector(dense_x.transpose(), sectype=secint)
     sec_dense_y = DenseVector(dense_y, sectype=secint)
 
+    print("===")
     start = datetime.now()
     z = sec_dense_x.dot(sec_dense_y)
     print(await mpc.output(z))
     end = datetime.now()
     delta_dense = end - start
-    print("===")
     print("Time for dense:", delta_dense.total_seconds())
 
-    # sec_x = SparseVector(x_sparse, secint)
-    # sec_y = SparseVector(y_sparse, secint)
-    # start = datetime.now()
-    # z = sec_x.dot(sec_y)
-    # print(await mpc.output(z))
-    # end = datetime.now()
-    # delta_sparse = end - start
-    # print("===")
-    # print("Time for sparse:", delta_sparse.total_seconds())
+    sec_x = SparseVector(x_sparse, secint)
+    sec_y = SparseVector(y_sparse, secint)
+    start = datetime.now()
+    z = sec_x.dot(sec_y)
+    print(await mpc.output(z))
+    end = datetime.now()
+    delta_sparse = end - start
+    print("===")
+    print("Time for sparse:", delta_sparse.total_seconds())
 
     # sec_x = SparseVectorNaive(x_sparse, secint)
     # sec_y = SparseVectorNaive(y_sparse, secint)
@@ -427,42 +426,42 @@ async def main():
 
     sec_x = SparseVectorNaivePSI(x_sparse, secint)
     sec_y = SparseVectorNaivePSI(y_sparse, secint)
+    print("===")
     start = datetime.now()
     z = await sec_x.dot(sec_y)
     print(await mpc.output(z))
     end = datetime.now()
     delta_sparse = end - start
-    print("===")
     print("Time for sparse psi:", delta_sparse.total_seconds())
 
     sec_x = SparseVectorNaivePSIOpti(x_sparse, secint)
     sec_y = SparseVectorNaivePSIOpti(y_sparse, secint)
+    print("===")
     start = datetime.now()
     z = await sec_x.dot(sec_y)
     print(await mpc.output(z))
     end = datetime.now()
     delta_sparse = end - start
-    print("===")
     print("Time for sparse psi optimized:", delta_sparse.total_seconds())
 
-    sec_x = SparseVectorORAM(x_sparse, secint)
-    sec_y = SparseVectorORAM(y_sparse, secint)
-    start = datetime.now()
-    z = await sec_x.dot(sec_y)
-    print(await mpc.output(z))
-    end = datetime.now()
-    delta_sparse = end - start
-    print("===")
-    print("Time for sparse ORAM:", delta_sparse.total_seconds())
+    # sec_x = SparseVectorORAM(x_sparse, secint)
+    # sec_y = SparseVectorORAM(y_sparse, secint)
+    # print("===")
+    # start = datetime.now()
+    # z = await sec_x.dot(sec_y)
+    # print(await mpc.output(z))
+    # end = datetime.now()
+    # delta_sparse = end - start
+    # print("Time for sparse ORAM:", delta_sparse.total_seconds())
 
     sec_x = SparseVectorQuicksort(x_sparse, secint)
     sec_y = SparseVectorQuicksort(y_sparse, secint)
+    print("===")
     start = datetime.now()
     z = await sec_x.dot(sec_y)
     print(await mpc.output(z))
     end = datetime.now()
     delta_sparse = end - start
-    print("===")
     print("Time for sparse quicksort:", delta_sparse.total_seconds())
 
 