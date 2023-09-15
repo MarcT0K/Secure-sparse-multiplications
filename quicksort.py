7edeada1049e3802a1ec7e35334c33b644de0d7a
DAMIE Marc <marc.damie@inria.fr>
2023-09-15 11:38:35 +0200

    Clean quicksort.py

--- quicksort.py
+++ quicksort.py
@@ -8,47 +8,8 @@ import random
 
 from mpyc.runtime import mpc
 
-from resharing import np_shuffle_3PC, shuffle_3PC
+from resharing import np_shuffle_3PC
 from shuffle import np_shuffle
-
-
-async def quicksort(sec_list, sectype, rec_call=False, key=None):
-    if not rec_call:
-        start = datetime.datetime.now()
-        mpc.random.shuffle(sectype, sec_list)
-        # if len(mpc.parties) == 3:
-        #     sec_list = await shuffle_3PC(sec_list)
-        # else:
-        #     mpc.random.shuffle(sectype, sec_list)
-        await mpc.barrier()
-        end = datetime.datetime.now()
-        print("shuffle time:", (end - start).total_seconds())
-
-    if 1 < len(sec_list):
-        p, curr_list = await partition(sec_list, key=key)
-        left_part = await quicksort(curr_list[:p], sectype, True, key=key)
-        right_part = await quicksort(curr_list[p + 1 :], sectype, True, key=key)
-        return left_part + [curr_list[p]] + right_part
-    else:
-        return sec_list
-
-
-async def partition(sec_list, key=None):
-    res = sec_list.copy()
-    i = -1
-    for j in range(len(res) - 1):
-        if key is None:
-            sec_comp = res[j] <= res[len(res) - 1]
-        else:
-            sec_comp = key(res[j]) <= key(res[len(res) - 1])
-
-        comp = await mpc.output(sec_comp)
-        if comp:
-            i += 1
-            res[i], res[j] = res[j], res[i]
-    p = i + 1
-    res[p], res[len(res) - 1] = res[len(res) - 1], res[p]
-    return p, res
 
 
 async def parallel_quicksort(sec_arr, key=None):
