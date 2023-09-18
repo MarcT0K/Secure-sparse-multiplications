9ceaa7fc0a7648b53b2dd9f82017eeed22bfe596
DAMIE Marc <marc.damie@inria.fr>
2023-09-18 20:23:30 +0200

    Download script for the datasets

---
+++ applications/download_datasets.sh
@@ -0,0 +1,15 @@
+sudo apt install wget unzip
+
+# Spam detection
+wget https://archive.ics.uci.edu/static/public/228/sms+spam+collection.zip
+unzip sms+spam+collection.zip
+rm readme sms+spam+collection.zip
+mv SMSSpamCollection spam.csv
+
+# Book recommendation application
+wget http://www2.informatik.uni-freiburg.de/~cziegler/BX/BX-CSV-Dump.zip
+unzip BX-CSV-Dump.zip
+rm BX-Books.csv BX-Users.csv BX-CSV-Dump.zip
+
+# Access log application
+wget https://raw.githubusercontent.com/pyduan/amazonaccess/master/data/train.csv > amazon.csv
