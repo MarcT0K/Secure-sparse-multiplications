29454a48188b0b0e0c0b52928ca531bb9d06b273
DAMIE Marc <marc.damie@inria.fr>
2023-09-21 10:15:18 +0200

    Implement secure logistic regression

--- applications/spam_detection.py
+++ applications/spam_detection.py
@@ -1,8 +1,17 @@
+import random
+
 import pandas as pd
 
 from mpyc.runtime import mpc
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import LogisticRegression
+
+from ..matrices import (
+    from_numpy_dense_matrix,
+    from_scipy_sparse_vect,
+    DenseVector,
+    SparseVector,
+)
 
 
 def extract_dataset():
@@ -20,17 +29,39 @@ def train_logreg(X, y):
     return logreg
 
 
-def experiment():
+class SecureLogisticRegression:
+    def __init__(self, logreg, sectype):
+        self.sectype = sectype
+        self.coef_ = from_numpy_dense_matrix(logreg.coef_[0], sectype)
+        self.intercept_ = sectype(logreg.intercept_[0])
+
+    async def predict(self, secure_input):
+        if isinstance(secure_input, SparseVector):
+            return (await secure_input.dot(self.coef_)) + self.intercept_
+        else:
+            return secure_input.dot(self.coef_) + self.intercept_
+
+
+async def experiment(sparse=True):
+    random.setstate(476528)
     sec_fxp = mpc.SecFxp(64)
 
     X_sparse, y = extract_dataset()
     model = train_logreg(X_sparse, y)
 
-    sec_coefs = sec_fxp.array(model.coef_[0])
-    sec_intercept = sec_fxp(model.intercept_[0])
+    sec_model = SecureLogisticRegression(model, sec_fxp)
 
-    # TODO: implement experiment
+    samples = random.sample(range(X_sparse.shape[0]), k=100)
+    for user_id in samples:
+        user_input = X_sparse[user_id, :]
+        if sparse:
+            sec_input = from_scipy_sparse_vect(user_input)
+        else:
+            sec_input = from_numpy_dense_matrix(user_input.todense())
+
+        sec_res = sec_model.predict(sec_input)
+        _res = await mpc.output(sec_res)
 
 
 if __name__ == "__main__":
-    experiment()
+    mpc.run(experiment())
