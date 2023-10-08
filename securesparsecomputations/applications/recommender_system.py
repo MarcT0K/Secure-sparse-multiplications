0013246b8b5db9c76389ad167a755aedb9a53df3
DAMIE Marc <marc.damie@inria.fr>
2023-10-08 13:16:25 +0200

    Change sampling parameter

--- securesparsecomputations/applications/recommender_system.py
+++ securesparsecomputations/applications/recommender_system.py
@@ -247,7 +247,7 @@ async def experiment():
         X_sparse = X_sparse[:NB_TRAINING_SAMPLES, :]
 
         if mpc.pid == 0:
-            samples = random.sample(range(101), k=5)
+            samples = random.sample(range(5000), k=5)
             # The last movies are mostly rated by the last users so we picked among
             # the first movies to have movies with at least one rating
         else:
